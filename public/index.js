bbff03b3ef7cbd23ebe163451d4d394d742b394d
Erin Mahoney <erinmahoneyphoto@gmail.com>
2018-11-18 08:38:22 -0800

    Build out circle functionality and enable score
    Randomize the shape of the circle and have it set to populate at the
    top of the board.
    
    When a user clicks on the circle, redraw the board to "remove" the
    circle and increment the score.
    
    Ensure that the score is reset when the game is reset or restarted.

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function() {
                 this.y = y;
                 this.radius = radius;
             }
-            update() {
+            draw() {
                 const circle = new Path2D();
                 circle.arc(
                     this.x,
@@ -20,11 +20,7 @@ window.addEventListener("load", function() {
                     2 * Math.PI,
                     false
                 );
-                ctx.stroke(circle);
-            }
-            draw() {
-                var circle = new Circle(100, 75, 50);
-                circle.update();
+                ctx.fill(circle);
             }
         }
 
@@ -32,12 +28,23 @@ window.addEventListener("load", function() {
             constructor(width, height) {
                 this.width = width;
                 this.height = height;
+                this.circles = [];
             }
             draw() {
                 ctx.canvas.width = this.width;
                 ctx.canvas.height = this.height;
             }
+            populate() {
+                const circle = new Circle(
+                    getRandom(0, 400),
+                    0,
+                    getRandom(10, 50)
+                );
+                board.circles.push(circle);
+                circle.draw();
+            }
             clear() {
+                this.circles = [];
                 ctx.clearRect(0, 0, this.width, this.height);
             }
         }
@@ -47,15 +54,20 @@ window.addEventListener("load", function() {
                 this.score = score;
                 this.board = board;
             }
-            newGame() {
-                board.draw();
-
+            setScore(count) {
+                this.score = count || 0;
                 const scoreBoard = document.getElementById("scoreBoard");
                 const score = `<h3>Score: ${this.score}</h3>`;
                 scoreBoard.innerHTML = score;
-
-                var circle = new Circle(100, 75, 50);
-                circle.draw();
+            }
+            newGame() {
+                board.draw();
+                this.setScore();
+                board.populate();
+            }
+            reset() {
+                this.setScore(0);
+                board.clear();
             }
         }
 
@@ -65,13 +77,36 @@ window.addEventListener("load", function() {
 
         const reset = document.getElementById("reset");
         reset.addEventListener("click", function() {
-            board.clear();
+            game.reset();
         });
 
         const start = document.getElementById("start");
         start.addEventListener("click", function() {
-            var circle = new Circle(100, 75, 50);
-            circle.draw();
+            game.reset();
+            board.populate();
         });
+
+        const container = document.getElementById("container");
+        container.addEventListener("click", function(e) {
+            var rect = e.target.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            const cx = board.circles.length ? board.circles[0].x : 0;
+            const cy = board.circles.length ? board.circles[0].y : 0;
+            const r = board.circles.length ? board.circles[0].radius : 0;
+            if (intersects(x, y, cx, cy, r)) {
+                game.setScore(1);
+                board.clear();
+            }
+        });
+    }
+    function intersects(x, y, cx, cy, r) {
+        var dx = x - cx;
+        var dy = y - cy;
+        return dx * dx + dy * dy <= r * r;
+    }
+    function getRandom(min, max) {
+        return Math.floor((Math.random() * max) + min);
     }
 });

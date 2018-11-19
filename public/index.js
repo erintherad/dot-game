bea063dd90d5cdf4df14adc1a93638ea2a7ab00a
Erin Mahoney <erinmahoneyphoto@gmail.com>
2018-11-18 16:34:34 -0800

    refactor code into classes

--- public/index.js
+++ public/index.js
@@ -1,16 +1,11 @@
 
 window.addEventListener("load", function() {
-    const canvas = document.getElementsByTagName("canvas")[0];
-    let ctx;
-    if (canvas.getContext) {
-        ctx = canvas.getContext("2d");
-    }
-
     class Circle {
-        constructor(x, y, radius) {
+        constructor(x, y, radius, ctx) {
             this.x = x;
             this.y = y;
             this.radius = radius;
+            this.ctx = ctx;
         }
         draw() {
             const circle = new Path2D();
@@ -22,7 +17,7 @@ window.addEventListener("load", function() {
                 2 * Math.PI,
                 false
             );
-            ctx.fill(circle);
+            this.ctx.fill(circle);
         }
         intersects(x, y, cx, cy) {
             var dx = x - cx;
@@ -40,10 +35,40 @@ window.addEventListener("load", function() {
             this.circleInterval = 0;
             this.animationInterval = 0;
             this.game = game;
+            this.ctx = null;
         }
         draw() {
-            ctx.canvas.width = this.width;
-            ctx.canvas.height = this.height;
+            const canvas = document.createElement("canvas");
+            canvas.id = "container";
+            const unsupportedText = document.createElement("p");
+            unsupportedText.innerText = (
+                "Sorry, your browser cannot display this game."
+            );
+            canvas.appendChild(unsupportedText);
+
+            this.ctx = canvas.getContext("2d");
+            this.ctx.canvas.width = this.width;
+            this.ctx.canvas.height = this.height;
+
+            document.body.append(canvas);
+
+            canvas.addEventListener("click", function(event) {
+                if (!this.game.activeGame) {
+                    return;
+                }
+                var rect = event.target.getBoundingClientRect();
+                const x = event.clientX - rect.left;
+                const y = event.clientY - rect.top;
+
+                for (var i = 0; i < this.circles.length; i ++) {
+                    const cx = this.circles.length ? this.circles[i].x : 0;
+                    const cy = this.circles.length ? this.circles[i].y : 0;
+                    if (this.circles[i].intersects(x, y, cx, cy)) {
+                        this.game.score.increment();
+                        this.circles.splice(i, 1);
+                    }
+                }
+            }.bind(this));
         }
         getRandomNumber(min, max) {
             return Math.floor((Math.random() * max) + min);
@@ -52,7 +77,8 @@ window.addEventListener("load", function() {
             const circle = new Circle(
                 this.getRandomNumber(0, 400),
                 this.getRandomNumber(0, -400),
-                this.getRandomNumber(10, 50)
+                this.getRandomNumber(10, 50),
+                this.ctx
             );
             this.circles.push(circle);
         }
@@ -64,14 +90,14 @@ window.addEventListener("load", function() {
         gameOver() {
             clearInterval(this.circleInterval);
             cancelAnimationFrame(this.animationInterval);
-            game.end();
+            this.game.end();
         }
         clear() {
             this.circles = [];
-            ctx.clearRect(0, 0, this.width, this.height);
+            this.ctx.clearRect(0, 0, this.width, this.height);
         }
         animate() {
-            ctx.clearRect(0, 0, this.width, this.height);
+            this.ctx.clearRect(0, 0, this.width, this.height);
 
             let circles = this.circles.length;
             let stopAnimate = false;
@@ -91,72 +117,109 @@ window.addEventListener("load", function() {
         }
     }
 
-    class Game {
+    class Button {
+        constructor(game, text, clickFunction) {
+            this.game = game;
+            this.text = text;
+            this.clickFunction = clickFunction;
+            this.element = null;
+        }
+        draw() {
+            this.element = document.createElement("button");
+            // Firefox persists the disabled state unless autocomplete is "off"
+            this.element.autocomplete = "off";
+            this.element.innerText = this.text;
+            document.body.append(this.element);
+            this.element.addEventListener("click", this.clickFunction.bind(this));
+        }
+        disable() {
+            this.element.disabled = true;
+        }
+        enable() {
+            this.element.disabled = false;
+        }
+    }
+
+    class Slider {
+        constructor(game, text, changeFunction) {
+            this.game = game;
+            this.text = text;
+            this.changeFunction = changeFunction;
+        }
+        draw() {
+            const div = document.createElement("div");
+            const slider = document.createElement("input");
+            slider.type = "range";
+            slider.min = this.game.board.speed;
+            slider.max = 5;
+            slider.step = 0.1;
+            slider.value = this.game.board.speed;
+            const label = document.createElement("label");
+            label.innerText = this.text;
+            div.appendChild(slider);
+            div.appendChild(label);
+            document.body.append(div);
+            slider.addEventListener("change", this.changeFunction.bind(this));
+        }
+    }
+
+    class Score {
         constructor() {
             this.score = 0;
+        }
+        draw() {
+            this.element = document.createElement("h2");
+            this.render();
+            document.body.append(this.element);
+        }
+        render() {
+            this.element.innerText = `Score: ${this.score}`;
+        }
+        reset() {
+            this.score = 0;
+            this.render();
+        }
+        increment() {
+            this.score += 1;
+            this.render();
+        }
+    }
+
+    class Game {
+        constructor() {
+            this.score = new Score();
+            this.resetButton = new Button(this, "Reset", function() {
+                this.game.reset();
+            });
+            this.startButton = new Button(this, "Start", function() {
+                this.disable();
+                this.game.reset();
+                this.game.board.populate();
+            });
+            this.speedSlider = new Slider(this, "Speed", function(event) {
+                const slider = event.target;
+                this.game.board.speed = parseInt(slider.value);
+            });
             this.board = new Board(400, 400, this);
             this.activeGame = true;
         }
-        setScore(count) {
-            count === 0 ? this.score = 0 : this.score += count;
-            const scoreBoard = document.getElementById("scoreBoard");
-            const score = `<h3>Score: ${this.score}</h3>`;
-            scoreBoard.innerHTML = score;
-        }
-        newGame() {
+        draw() {
+            this.score.draw();
+            this.startButton.draw();
+            this.resetButton.draw();
+            this.speedSlider.draw();
             this.board.draw();
-            this.setScore(0);
-            this.board.populate();
         }
         end() {
             this.activeGame = false;
+            this.startButton.enable();
         }
         reset() {
             this.activeGame = true;
-            this.setScore(0);
+            this.score.reset();
             this.board.clear();
         }
     }
 
-    const game = new Game();
-    game.newGame();
-
-    const reset = document.getElementById("reset");
-    reset.addEventListener("click", function() {
-        game.reset();
-    });
-
-    const start = document.getElementById("start");
-    start.addEventListener("click", function() {
-        game.reset();
-        game.board.populate();
-    });
-
-    const container = document.getElementById("container");
-    container.addEventListener("click", function(e) {
-        if (!game.activeGame) {
-            return;
-        }
-        var rect = e.target.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        for (var i = 0; i < game.board.circles.length; i ++) {
-            const cx = game.board.circles.length ? game.board.circles[i].x : 0;
-            const cy = game.board.circles.length ? game.board.circles[i].y : 0;
-            if (game.board.circles[i].intersects(x, y, cx, cy)) {
-                game.setScore(1);
-                game.board.circles.splice(i, 1);
-            }
-        }
-    });
-
-    const slider = document.getElementById("slider");
-    slider.min = game.board.speed;
-    slider.max = 5;
-    slider.value = 1;
-    slider.addEventListener("change", function() {
-        console.log("changed")
-        game.board.speed = parseInt(slider.value);
-    });
+    (new Game()).draw();
 });
